1e6f1ec402c8f0b30fe48f1dfc2b56c983a07f89
ccc25687 <44232924+ccc25687@users.noreply.github.com>
2018-12-18 19:32:29 +0800

    Initial commit

---
+++ Assets/c/analogy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class analogy : MonoBehaviour {
+
+    public RectTransform handle;
+    public RectTransform rect;
+    private bool dragging = false;
+
+
+    public float speed = 6;
+    public Transform obj;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GameObject.Find("chicken").GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Drog();
+    }
+
+    public void StartDrag() {
+        dragging = true;
+        animator.SetBool("run",true);
+    }
+
+    public void Drog() {//執行拖曳中
+        if (!dragging) { return; }
+        Vector2 newPos = (Vector2)Input.mousePosition - rect.anchoredPosition;
+        Vector2 pos = Vector2.ClampMagnitude(newPos, 42f);
+        handle.anchoredPosition = pos;
+
+        //物體移動
+        Vector2 dir = (pos / 42f) * speed * Time.deltaTime;
+        state(newPos);//物體轉向
+
+        animator.speed = newPos.magnitude / 42f;
+
+        obj.Translate(dir);
+    }
+
+    public void StopDrag() {
+        dragging = false;
+        handle.anchoredPosition = Vector2.zero;
+        animator.SetBool("run", false);
+        animator.speed = 1f;
+    }
+
+    private void state(Vector2 newPos){//物體轉向
+        if(newPos.x>0f)
+        obj.transform.localScale = new Vector3(-1f, 1f, 1f);//右
+        else
+        obj.transform.localScale = new Vector3(1f, 1f, 1f);//左
+    }
+}
---
+++ Assets/c/jump.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+
+    private bool jumping = false;//跳躍中
+    private bool isGround = false;//在不在地板
+
+    public float jumpHeight=1000f;
+    private Rigidbody2D player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("chicken").GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(jumping);
+        Debug.Log(isGround);
+        SetJumpState();
+        StateMachine();
+    }
+
+    public void Jump() {
+        if (!isGround || player.velocity.y != 0) { return; }
+
+        player.AddForce(Vector3.up*jumpHeight);
+
+    }
+
+    void SetJumpState() {
+        if (player.velocity.y > 0.1) {
+            isGround = false;
+            jumping = true;
+        } else if (jumping) {
+            jumping = false;
+        }
+    }
+
+    void StateMachine() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D co)
+    {
+        if (co.gameObject.tag == "Ground" && co.contacts[0].normal ==Vector2.up) {
+            isGround = true;
+        }
+    }
+}
